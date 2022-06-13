61a138b930650c82ff62ae4396c8cf8e05485b84
NAUMAN IFTIKHAR SATTI <47976829+NaumanSatti@users.noreply.github.com>
2022-06-13 23:12:57 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=dcc-i=21%h)xa(cd6lc=xcaum^=0&)+0f&v4iay#hfev*yx@#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sindh-hw.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','sindhhw.herokuapp.com']
 
 
 # Application definition
